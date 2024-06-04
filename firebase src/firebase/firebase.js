7b4d53d1bb3531b04ef64f21daa8141fc1923d5f
sukhjot-000 <156485481+sukhjot-000@users.noreply.github.com>
2024-06-04 08:53:50 -0400

    Update firebase.js

--- firebase src/firebase/firebase.js
+++ firebase src/firebase/firebase.js
@@ -4,12 +4,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjNv_-Uq-aVDL325MF2a1r_vQSyz8xpVU",
-  authDomain: "authdemo-18977.firebaseapp.com",
-  projectId: "authdemo-18977",
-  storageBucket: "authdemo-18977.appspot.com",
-  messagingSenderId: "1066747413866",
-  appId: "1:1066747413866:web:29fe2a4fa3f6a6583b5113",
+
 };
 
 // Initialize Firebase
